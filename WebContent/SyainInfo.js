578c481145761aebd12b3250839081b67afe0104
ryotaro_shimura <ry-shimura@virtualex.co.jp>
2020-04-23 18:12:00 +0900

    ４月２３日

--- WebContent/SyainInfo.js
+++ WebContent/SyainInfo.js
@@ -1,5 +1,15 @@
-var aaa = 1;
-var bb  = 2;
 
-var c = aaa + bb;
-console.log(c);
+
+
+
+$.ajax({
+	type:'GET',
+	url:'/kisoteichaku/SyainInfo',
+	dataType:'json',
+	data requestData,
+
+	success: function(json){
+		console.log(json);
+
+	}
+});
--- src/kisoteichaku/SyainInfo.java
+++ src/kisoteichaku/SyainInfo.java
@@ -1,5 +1,53 @@
 package kisoteichaku;
 
-public class SyainInfo {
+import java.io.Serializable;
+
+public class SyainInfo implements Serializable {
+
+	private String syainId;
+	private String syainName;
+	private String syainAge;
+	private String syainGender;
+	private String syainAddress;
+	private String busyoId;
+
+	public String getSyainId() {
+		return syainId;
+	}
+	public void setSyainId(String syainId) {
+		this.syainId = syainId;
+	}
+	public String getSyainName() {
+		return syainName;
+	}
+	public void setSyainName(String syainName) {
+		this.syainName = syainName;
+	}
+	public String getSyainAge() {
+		return syainAge;
+	}
+	public void setSyainAge(String syainAge) {
+		this.syainAge = syainAge;
+	}
+	public String getSyainGender() {
+		return syainGender;
+	}
+	public void setSyainGender(String syainGender) {
+		this.syainGender = syainGender;
+	}
+	public String getSyainAddress() {
+		return syainAddress;
+	}
+	public void setSyainAddress(String syainAddress) {
+		this.syainAddress = syainAddress;
+	}
+	public String getBusyoId() {
+		return busyoId;
+	}
+	public void setBusyoId(String busyoId) {
+		this.busyoId = busyoId;
+	}
+
+
 
 }
