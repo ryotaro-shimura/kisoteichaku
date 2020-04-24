3d0b2790cad02102550f992602e4cd7364747e72
ryotaro_shimura <ry-shimura@virtualex.co.jp>
2020-04-24 10:32:59 +0900

    js、htmlの編集

--- WebContent/SyainInfo.js
+++ WebContent/SyainInfo.js
@@ -4,12 +4,23 @@
 
 $.ajax({
 	type:'GET',
-	url:'/kisoteichaku/SyainInfo',
+	url:'/kisoteichaku/SyainList',
 	dataType:'json',
 	data requestData,
 
 	success: function(json){
 		console.log(json);
 
+		for(var i=0; i<json.length; i++){
+			var row = '<tr>'+
+			'<td>'+json[i].syainId+'</td>'+
+			'<td>'+json[i].syainName+'</td>'+
+			'<td>'+'<button id="syain_edit">編集</button>'+'</td>'+
+			'<td>'+'<button id="syain_delete">削除</button>'+'</td>'+
+			'</tr>';
+
+			$('#table_data').append(row);
+		}
 	}
 });
+
